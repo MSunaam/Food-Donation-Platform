4411a3d984ce8e76350f925365ec691b3fe8245e
MSunaam <95274859+MSunaam@users.noreply.github.com>
2023-05-11 12:43:06 +0500

    home page

--- donation-website/resources/views/welcome.blade.php
+++ donation-website/resources/views/welcome.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>
+            MealShare
+        </title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/food-bank.png') }}">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,23 +18,54 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
+        <style>
+            #text-over-img{
+                margin-top: -500px;
+            }
+        </style>
 
     </head>
 
     <body>
-        <div class="container-fluid mx-3">
-            <nav class="navbar navbar-light navbar-bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="">
-                        <img src="images/food-bank.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                        MealShare
-                    </a>
-                    <div class="alert alert-danger">
-                        <strong>Warning!</strong> This is a demo site. Do not enter any sensitive information.
-                    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/food-bank.png') }}" alt="mealShare" width="30" height="24" class="d-inline-block align-text-top">
+                MealShare
+            </a>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Contact Us</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <button class="btn btn-success mx-3 px-3" id="loginButton">Login</button>
+                    <button class="btn btn-outline-success" id="signUpButton">Sign Up</button>
                 </div>
-            </nav>
+            </div>
         </div>
+    </nav>
+
+    <div class="container-fluid">
+        <img src="{{ asset('images/home-image.jpg') }}" class="img-fluid d-block" style="max-width: 80%; z-index: 0">
+        <div class="col-md-4"  id="text-over-img">
+            <p class="carousel-caption h1 text-end" style="z-index: 1; color: black;">Donate to save lives</p>
+        </div>
+    </div>
+
     </body>
 
 </html>
