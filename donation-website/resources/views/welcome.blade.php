001d0e044e5d204577ec800f0f32bde5311eaaba
MSunaam <95274859+MSunaam@users.noreply.github.com>
2023-05-11 14:17:51 +0500

    css

--- donation-website/resources/views/welcome.blade.php
+++ donation-website/resources/views/welcome.blade.php
@@ -16,16 +16,11 @@
 
         <!-- Styles -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="{{ asset('css/home.css') }}" rel="stylesheet" type="text/css">
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-
-        <style>
-            #text-over-img{
-                margin-top: -500px;
-            }
-        </style>
 
     </head>
 
@@ -60,9 +55,10 @@
     </nav>
 
     <div class="container-fluid">
-        <img src="{{ asset('images/home-image.jpg') }}" class="img-fluid d-block" style="max-width: 80%; z-index: 0">
-        <div class="col-md-4"  id="text-over-img">
-            <p class="carousel-caption h1 text-end" style="z-index: 1; color: black;">Donate to save lives</p>
+        <img src="{{ asset('images/home-image.jpg') }}" class="img-fluid d-block" style="max-width: 80%; z-index: 0" alt="Image">
+        <div id="text-over-img">
+            <p class="carousel-caption h1 text-end" id="home-pic-caption">Donate to save lives</p>
+            <button class="btn btn-primary">Get Started</button>
         </div>
     </div>
 
