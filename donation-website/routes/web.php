25f6ad23f657da7c9daa3025f9639e96841fc1d5
MSunaam <95274859+MSunaam@users.noreply.github.com>
2023-05-16 20:20:51 +0500

    update

--- donation-website/routes/web.php
+++ donation-website/routes/web.php
@@ -34,13 +34,13 @@ Route::controller(UserController::class)->group(function (){
     Route::post('/updateProfile', 'updateProfile')->name('updateProfile');
 })->middleware('auth');
 
-Route::get('/dashboard', [FoodItemController::class, 'getCategoryInfo'])->name('dashboard')->middleware('auth');
+Route::controller(FoodItemController::class)->group(function (){
+    Route::get('/dashboard', 'getCategoryInfo')->name('dashboard');
+    Route::get('/updateInfo', 'getCategoryInfo')->name('refreshData');
+    Route::post('/add_item', 'add')->name('add_item');
+})->middleware('auth');
 
-Route::get('/updateInfo', [FoodItemController::class, 'getCategoryInfo'])->name('refreshData')->middleware('auth');
-
-Route::post('/add_item', [FoodItemController::class, 'add'])->name('add_item')->middleware('auth');
-
-Route::post('/add_donation', [DonationController::class, 'addDonation'])->name('add_donation')->middleware('auth');
-
-Route::post('/mark_schedule', [DonationController::class, 'markComplete'])->name('mark_schedule')->middleware('auth');
-//Route::get('/test', [])
+Route::controller(DonationController::class)->group(function (){
+    Route::post('/add_donation','addDonation')->name('add_donation');
+    Route::post('/mark_schedule','markComplete')->name('mark_schedule');
+})->middleware('auth');
